4afb37c7a08633f83d16f24db31b1d2052c85ff3
WalukaUI <cwaluka@yahoo.com>
2021-03-15 21:16:00 -0700

    Passing 7 tests

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+const employee ={name:"Luka",
+                 streetAddress:"122 Normandy"
+                };
+
+              
+function updateEmployeeWithKeyAndValue( employee, key ,street){
+    for (const key in employee) {
+        delete employee[key];}
+        employee.name = 'Sam';
+        const newEmployee={...employee}
+        newEmployee[key]=street;
+        return newEmployee;
+}
+updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key ,street){
+    for (const key in employee) {
+        delete employee[key];}
+        employee.name = 'Sam';
+       
+        employee[key]=street;
+        return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway');
+
+function deleteFromEmployeeByKey(employee, name){
+    for (const key in employee) {
+        delete employee[key];}
+        employee.name = 'Sam';
+
+        const newEmployee={...employee}
+        delete newEmployee.name;
+        return newEmployee;
+}
+deleteFromEmployeeByKey(employee, "name");
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    for (const key in employee) {
+        delete employee[key];}
+        employee.name = 'Sam';
+
+        delete employee.name;
+        return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee, "name")
